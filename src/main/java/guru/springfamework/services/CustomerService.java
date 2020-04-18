62de3621896ebe0e495025da19953db6ea1a9559
y00562359 <ylozsoy@gmail.com>
2020-04-18 21:36:00 +0300

    386) Added an integration test

--- src/main/java/guru/springfamework/services/CustomerService.java
+++ src/main/java/guru/springfamework/services/CustomerService.java
@@ -16,4 +16,6 @@ public interface CustomerService {
     CustomerDTO getCustomerById(Long id);
 
     CustomerDTO getCustomerByFirstName(String firstName);
+
+    CustomerDTO patchCustomer(Long id, CustomerDTO customerDTO);
 }
--- src/main/java/guru/springfamework/services/CustomerServiceImpl.java
+++ src/main/java/guru/springfamework/services/CustomerServiceImpl.java
@@ -83,4 +83,20 @@ public class CustomerServiceImpl implements CustomerService {
     public CustomerDTO getCustomerByFirstName(String firstName) {
         return customerMapper.customerToCustomerDTO(customerRepository.findByFirstName(firstName));
     }
+
+    @Override
+    public CustomerDTO patchCustomer(Long id, CustomerDTO customerDTO) {
+        return customerRepository.findById(id).map(customer -> {
+
+            if(customerDTO.getFirstName() != null){
+                customer.setFirstName(customerDTO.getFirstName());
+            }
+
+            if(customerDTO.getLastName() != null){
+                customer.setLastName(customerDTO.getLastName());
+            }
+
+            return customerMapper.customerToCustomerDTO(customerRepository.save(customer));
+        }).orElseThrow(RuntimeException::new); //todo implement better exception handling;
+    }
 }
