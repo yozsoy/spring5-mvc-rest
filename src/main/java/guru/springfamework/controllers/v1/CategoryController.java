81ec84d3a724fe56acf556500eca286a5ca45406
y00562359 <ylozsoy@gmail.com>
2020-04-19 02:45:50 +0300

    391) spring 4 @RestController annotation has been utilized in one controller

--- src/main/java/guru/springfamework/controllers/v1/CategoryController.java
+++ src/main/java/guru/springfamework/controllers/v1/CategoryController.java
@@ -6,14 +6,12 @@ import guru.springfamework.services.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by jt on 9/26/17.
  */
-@Controller
+@RestController
 @RequestMapping(CategoryController.BASE_URL)
 //@RequestMapping(${some.url.value}) -> this way the value comes from application.properties file
 //but we would need to initiate whole spring context to get the value which is bad for our tests
@@ -27,17 +25,14 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping
-    public ResponseEntity<CatergoryListDTO> getallCatetories(){
-
-        return new ResponseEntity<CatergoryListDTO>(
-                new CatergoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public CatergoryListDTO getallCatetories() {
+        return new CatergoryListDTO(categoryService.getAllCategories());
     }
 
     @GetMapping("{name}")
-    public ResponseEntity<CategoryDTO> getCategoryByName( @PathVariable String name){
-        return new ResponseEntity<CategoryDTO>(
-                categoryService.getCategoryByName(name), HttpStatus.OK
-        );
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDTO getCategoryByName(@PathVariable String name) {
+        return categoryService.getCategoryByName(name);
     }
 }
